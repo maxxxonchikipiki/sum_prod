088f5a5914c76cd4153bea8d4f0b2ae724d17423
st140471 <st140471@student.spbu.ru>
2025-10-21 11:50:12 +0300

    add solution

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+    unsigned int number;
+    std::cout << "Input number: ";
+    std::cin >> number;
+
+    unsigned int summ=0;
+    unsigned int prod=1U;
+
+    do{
+	summ += number % 10;
+	prod *= number % 10;
+	number /= 10;
+    }while(number != 0);
+
+    std::cout << summ << ',' << ' '<<  prod<< std::endl;
+
+    return 0;
+}
